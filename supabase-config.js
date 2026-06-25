e0248bb682e5fa606979e8521b7195ef6c888470
taylorlayork <taylorlayork@yahoo.com>
2026-06-25 10:09:16 -0500

    Add files via upload

--- supabase-config.js
+++ supabase-config.js
@@ -1,6 +1,6 @@
 // Paste your Supabase project values here, then upload this file with the rest of the site.
 // These are the public browser values from Supabase Project Settings > Data API.
 window.ACROSS_SUPABASE_CONFIG = {
-  url: 'https://soleubzyrhryyhbahoyq.supabase.co/rest/v1/',
+  url: 'https://soleubzyrhryyhbahoyq.supabase.co',
   anonKey: 'sb_publishable_EFgWMh56GR_AGezRrCUJtA_J3uOtMlj'
 };
